201335417c1f4fe36578b7dc03c7854866b25a56
NatalliaDrozd <nat.dr000@gmail.com>
2022-06-19 14:01:18 +0300

    init project

---
+++ home_work_23/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  home_work_23
+//
+//  Created by Natalia Drozd on 19.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
