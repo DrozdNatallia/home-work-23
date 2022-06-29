48469934c523a2b4fe37d5987c21459e50f34e25
NatalliaDrozd <nat.dr000@gmail.com>
2022-06-30 01:54:41 +0300

    extension moved to a separate file

--- home_work_23/Cells/HourlyWeayherCell/HourlyWeatherCell.swift
+++ home_work_23/Cells/HourlyWeayherCell/HourlyWeatherCell.swift
@@ -27,21 +27,3 @@ class HourlyWeatherCell: UITableViewCell {
     }
     
 }
-
-extension HourlyWeatherCell: UICollectionViewDelegate, UICollectionViewDataSource {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return tempArray.count
-    }
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HourlyWeatherCollectionCell.key, for: indexPath) as? HourlyWeatherCollectionCell {
-            guard let tempArray = tempArray, let dtArray = dtArray, let imageArray = imageArray else {return cell }
-            cell.hours.text = dtArray[indexPath.row].description
-            cell.temp.text = "\(tempArray[indexPath.row])°"
-            cell.imageWeayherView.image = imageArray[indexPath.row]
-            
-            return cell
-        }
-        return UICollectionViewCell()
-    }
-}
