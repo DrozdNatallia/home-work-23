7f89b300f00c09bb4bbef8113847b7755855a6f6
NatalliaDrozd <nat.dr000@gmail.com>
2022-07-04 13:14:17 +0300

    changed update table, added deinit

--- home_work_23/ViewControllers/RealmListController.swift
+++ home_work_23/ViewControllers/RealmListController.swift
@@ -23,11 +23,24 @@ class RealmListController: UIViewController {
             guard let tableView = self?.tableViewRequest else { return }
             switch changes {
             case .initial: break
-            case .update:
-                tableView.reloadData()
+            case .update(_, let deletions, let insertions, let modifications):
+                tableView.performBatchUpdates({
+                   tableView.deleteRows(at: deletions.map({ IndexPath(row: $0, section: 0)}),
+                                        with: .automatic)
+                    tableView.insertRows(at: insertions.map({ IndexPath(row: $0, section: 0) }),
+                                         with: .automatic)
+                    tableView.reloadRows(at: modifications.map({ IndexPath(row: $0, section: 0) }),
+                                         with: .automatic)
+                })
             case .error(let error):
                 fatalError("\(error)")
             }
         }
     }
+    
+    
+    deinit {
+        notificationToken?.invalidate()
+    }
+
 }
